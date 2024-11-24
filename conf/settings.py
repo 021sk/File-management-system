7b64a2087f20c725ecdebfbd15364c96ae2527b3
shahab <shahaab.kabiri@gmail.com>
2024-11-24 07:52:57 -0800

    :rocket: start DRf

--- conf/settings.py
+++ conf/settings.py
@@ -37,8 +37,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account',
     'file',
+    'rest_framework',
 
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- file/urls.py
+++ file/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'file'
 urlpatterns = [
+    # view
     path('', views.DashboardView.as_view(), name='home'),
     path('/folder/<int:folder_id>/', views.FolderDetailView.as_view(), name='folder_detail'),
 
+]+[
+    # api_view
+
 ]
+
